5c6824e4060e7e1f207443985a01c5c8cb3fca81
Ayabelhaj <abelhajb@gmail.com>
2022-09-22 15:56:42 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,22 +1,13 @@
-<<<<<<< HEAD
-include < stdio.h >
-/**
- * main - main block
- *Return: 0
- */
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-=======
 #include <stdio.h>
+
 /**
- * main - main block
- * Return: 0
- */
+ *  * main - prints the phrase with proper grammar,
+ *   * but the outcome is a piece of art,
+ *    * Return: 0 if exited properly, non-zero otherwise
+ *     */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
->>>>>>> 373a7e963c01e0dd65de72347cc574d12d9b893e
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
 }
+
